c369798ee6637726015403cc6b1ae2f11da24450
linyan <liya20150602@163.com>
2019-07-17 16:39:06 +0800

    modfiy

---
+++ LinyanBlock/src/index.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import styles from './style.css';
+
+const Block: React.FC = () => {
+  return (
+    <div className={styles.normal}>
+      <h1>I am a umi block!</h1>
+	  <div className={styles.block}>hello world!!!</div>
+    </div>
+  );
+}
+
+export default Block;
