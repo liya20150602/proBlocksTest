f10442b4f8f07f011b859598ef2978c6a37d9862
linyan <liya20150602@163.com>
2019-07-18 09:48:18 +0800

    修改为typeScript构建页面

--- LinyanBlock/src/index.tsx
+++ LinyanBlock/src/index.tsx
@@ -1,12 +1,21 @@
-import React from 'react';
-import styles from './index.css';
+import React, { PureComponent } from 'react';
+// import styles from './index.css';
 
-const Block: React.FC = () => {
-  return (
-    <div className={styles.normal}>
+// const Block: React.FC = () => {
+//   return (
+//     <div className={styles.normal}>
+//       <h1>I am a umi block!</h1>
+//     </div>
+//   );
+// }
+class Block1 extends PureComponent<any> {
+  render(){
+    return(
+      <div>
       <h1>I am a umi block!</h1>
-    </div>
-  );
-}
+      </div>
+    )
+  }
 
-export default Block;
+}
+export default Block1;
